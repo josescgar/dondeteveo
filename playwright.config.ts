63797b667865d44e3a266e0bc8734393eb85f269
Jose Escobar <jose.escobar.dev@gmail.com>
2026-03-08 01:02:38 +0100

    Add pre-push hook. Run e2e tests on mobile

--- playwright.config.ts
+++ playwright.config.ts
@@ -17,6 +17,10 @@ export default defineConfig({
       name: "chromium",
       use: { ...devices["Desktop Chrome"] },
     },
+    {
+      name: "mobile-chromium",
+      use: { ...devices["Pixel 5"] },
+    },
   ],
   webServer: {
     command: `npm run dev -- --host 127.0.0.1 --port ${PORT}`,
